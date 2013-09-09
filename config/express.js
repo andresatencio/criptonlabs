20396e7fb30f184296f9ec5b130f652f2fe644f6
Ivan Manetta <ivanmanetta@gmail.com>
2013-09-09 15:33:56 -0300

    remove express config from app.js and add in config/

---
+++ config/express.js
@@ -0,0 +1,20 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..');
+
+
+module.exports = function(app, express) {
+  app.configure(function(){
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', rootPath + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('your secret here'));
+    app.use(express.session());
+    app.use(app.router);
+    app.use(express.static(rootPath + '/public'));
+  });
+
+};
