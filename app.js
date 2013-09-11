51fda2c78e9966c1844c67e9bca0dea55f858db9
Andres Atencio <and.atencio@gmail.com>
2013-09-11 16:54:29 -0300

    correcion mailto

--- app.js
+++ app.js
@@ -20,7 +20,4 @@ app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
-  var fruits = ["Banana", "Orange", "Apple", "Mango"];
-  console.log(fruits)
-  console.log(fruits.reverse());
 });
